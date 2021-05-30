99f5b8bef5ad4f775734e97d969ff2d3fde82c6d
Azhar Ali <azharofficial599@gmail.com>
2021-05-31 01:14:10 +0530

    commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {AppearanceProvider} from 'react-native-appearance';
+import {ThemeProvider} from './theme/ThemeProvider';
+import {Screen, Switch, Message} from './components';
+
+export default function App() {
+  return (
+    <AppearanceProvider>
+      <ThemeProvider>
+        <Screen>
+          <Switch />
+          <Message />
+        </Screen>
+      </ThemeProvider>
+    </AppearanceProvider>
+  );
+}
